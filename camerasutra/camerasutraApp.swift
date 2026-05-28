e2e84e8e8de8f37ed0df880952ed6ce1b8cc11f2
manorajesh <mano@raje.sh>
2026-05-28 12:37:15 -0700

    Initial Commit

---
+++ camerasutra/camerasutraApp.swift
@@ -0,0 +1,17 @@
+//
+//  camerasutraApp.swift
+//  camerasutra
+//
+//  Created by Mano Rajesh on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct camerasutraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
